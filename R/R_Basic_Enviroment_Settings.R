8d104ba1612d4740795561f64dc34b091b69e5c0
AsyncBridge <jm84.joo@gmail.com>
2016-11-15 03:45:28 +0900

    Update R script
    Basic Enviroment Settings R script

---
+++ R/R_Basic_Enviroment_Settings.R
@@ -0,0 +1,29 @@
+# Install a package
+install.packages('ggplot2')
+
+# Update installed packages 
+#update.packages()
+
+# Remove an installed package
+#remove.packages('ggplot2')
+
+# Use ggplot2 library
+library(ggplot2)
+
+# Get current working directory
+getwd()
+
+# Set current working directory
+setwd('C:/Users/Test/Documents')
+
+# Use help
+# help shows Description, Usage, Arguments, Value, Note, See Also and Examples.
+help(getwd)
+?getwd
+
+# Search a specific string in help
+help.search('getwd')
+??getwd
+
+# See an example
+example('getwd')
